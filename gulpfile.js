1990def16b1795a497e43a3e660fc421a71933bb
Brigitte <brigitte.cheetham@gmail.com>
2018-11-26 22:24:52 +0200

    Create style modules folder and add add large-hero and btn files

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssimport = require('postcss-import');
+
+gulp.task('default', function() {
+	console.log('hooray, my first gulp task and i understand what I have done!');
+});
+
+gulp.task('html', function() {
+	console.log('imagine something useful done to html');
+});
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+}); 
+
+gulp.task('watch', function() {
+	watch('./app/index.html', function() {
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	});
+})
